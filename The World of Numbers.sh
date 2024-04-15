ecccba5f7272d19fcbd06a0b0598e2b64c180493
Indeevar1019 <95519680+Indeevar1019@users.noreply.github.com>
2024-04-15 07:49:13 +0530

    Create The World of Numbers.sh

---
+++ The World of Numbers.sh
@@ -0,0 +1,26 @@
+Given two integers,  and , find their sum, difference, product, and quotient.
+
+Input Format
+
+Two lines containing one integer each ( and , respectively).
+
+Constraints
+
+
+
+Output Format
+
+Four lines containing the sum (), difference (), product (), and quotient (), respectively.
+(While computing the quotient, print only the integer part.)
+
+
+
+read x
+read y
+echo $[x+y]
+
+echo $[x-y]
+
+echo $[x*y]
+
+echo $[x/y]
